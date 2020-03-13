0163d128c9f80e72f5b8b5b404dcb873c172e10e
Charlie Bayne <charlie.bayne@kaleido.com>
2020-03-13 14:49:18 -0400

    updated to work better given diverse input types

--- R/heatmapper.R
+++ R/heatmapper.R
@@ -11,34 +11,35 @@
 #' @examples
 heatmapper = function(annotated_data,labels = "Community"){
 
-#Selecting just the numeric data
-numeric_data = annotated_data %>%
-    dplyr::select(od600_lag_length:length(annotated_data))
+    #Selecting just the numeric data
+    numeric_data = annotated_data %>%
+        dplyr::select_if(is.numeric)
 
-#Scaling the numeric data
-scaled_data = sapply(numeric_data,scale)
+    #Scaling the numeric data
+    scaled_data = sapply(numeric_data,scale)
 
-#Selecting just the categorical data
-Sample_Labels = annotated_data %>%
-    dplyr::select(1:(length(annotated_data)-10))
+    #Selecting just the categorical data
+    Sample_Labels = annotated_data %>%
+        dplyr::select_if(function(x){is.factor(x) | is.character(x)})
 
-#Setting row names to be sample.ID
-row.names(Sample_Labels) = Sample_Labels$Sample.ID
+    #Setting row names to be sample.ID
+    row.names(Sample_Labels) = Sample_Labels$Sample.ID
 
-all = cbind(Sample_Labels,scaled_data)
+    all = cbind(Sample_Labels,scaled_data)
 
-#Setting the row names to be Sample.IDs
-row.names(scaled_data) = all$Sample.ID
+    #Setting the row names to be Sample.IDs
+    row.names(scaled_data) = all$Sample.ID
 
-#Setting up custom hovertext to contain compound information
-compound_vector = rep(Sample_Labels$Compound,length(numeric_data))
+    #Setting up custom hovertext to contain compound information
+    compound_vector = rep(Sample_Labels$Compound,length(numeric_data))
 
-hover_text = matrix(compound_vector, nrow = length(Sample_Labels$Compound), ncol = length(numeric_data))
+    hover_text = matrix(compound_vector, nrow = length(Sample_Labels$Compound), ncol = length(numeric_data))
 
-#Plotting the interactive heatmap
-heatmaply::heatmaply(scaled_data,dist_method = "manhattan",row_side_colors = Sample_Labels[,labels],
-                     cexRow = 0.1,cexCol = 0.7,custom_hovertext = hover_text)
+    #Plotting the interactive heatmap
+    heatmaply::heatmaply(scaled_data,dist_method = "manhattan",row_side_colors = Sample_Labels[,labels],
+                         cexRow = 0.1,cexCol = 0.7,custom_hovertext = hover_text)
 
+}
 }
 
 
